b43dc0578f6982b00241a0b954dfcdb89b3ceed6
Vivek Singh <viveksingh9301@gmail.com>
2025-12-02 21:26:58 +0530

    Refactor heading rendering to use JSX instead of React.createElement

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Using React.createElement
 const heading = React.createElement("h1", {}, "Hello world - a message from React!");
+
+// Using JSX
+const headingWithJSX = <h1>Hello world - made using JSX!</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(headingWithJSX);
