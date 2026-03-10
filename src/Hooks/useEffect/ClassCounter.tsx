d50bc7ae69d5d92332a7a5314b646e56392352a5
Vivek Singh <viveksingh9301@gmail.com>
2026-03-10 18:31:57 +0530

    Hooks: demonstration of conditional re-rendering of component using componentDidUpdate in ClassCounter

--- src/Hooks/useEffect/ClassCounter.tsx
+++ src/Hooks/useEffect/ClassCounter.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 type State = {
   count: number;
+  name: string;
 };
 
 class ClassCounter extends Component<{}, State> {
@@ -9,6 +10,7 @@ class ClassCounter extends Component<{}, State> {
     super(props);
     this.state = {
       count: 0,
+      name: "",
     };
   }
 
@@ -17,13 +19,24 @@ class ClassCounter extends Component<{}, State> {
   }
 
   componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<State>): void {
-    document.title = `Clicked ${this.state.count} times`;
+    // Here, we are conditionally re-render the component
+    // only if the user clicks the button and the count's value changes
+    // and, not when the text-input value changes.
+    if (this.state.count != prevState.count) {
+      console.log("Updating document title");
+      document.title = `Clicked ${this.state.count} times`;
+    }
   }
 
   render() {
     return (
       <div>
         <h2>Using Class component.</h2>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ name: e.target.value })}
+        />
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>Click</button>
       </div>
     );
