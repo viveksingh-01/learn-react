f968ae303aab3a99c23e457cae9224a0340120c1
Vivek Singh <viveksingh9301@gmail.com>
2026-03-12 21:31:12 +0530

    Hooks: useCallback - demonstrate the need for useCallback hook to cache the function

--- src/Hooks/useCallback/ParentComponent.tsx
+++ src/Hooks/useCallback/ParentComponent.tsx
@@ -29,6 +29,23 @@ const ParentComponent = () => {
 export default ParentComponent;
 
 // NOTE:
+// After applying React.memo, we have prevented re-rendering of Title and Count components
+// but we can see that the Button component for incrementing salary still re-renders when the one
+// to increment the age is clicked and vice-versa.
+// REASON:
+// Whenever we click on the button to increment age, the ParentComponent re-renders with the new age-state,
+// it creates a new 'incrementSalary' function (and incrementAge as well) upon every re-render.
+// When dealing with functions, we always need to consider the reference-equality (like Objects), this means
+// the function before the re-render is not same as the function created after the re-render.
+// and since we are passing this new function to handleClick prop of the Button component, React.memo treats
+// it as a change in props and hence re-renders the Button component.
+
+// Now, how do we tell React that we don't need to create a new 'incrementSalary' function when we increment
+// the age, and vice-versa? The answer is useCallback hook!
+
+// ----------------------------------------------------------------------------------------------------------
+
+// OLDER NOTES:
 // Here, we can observe that upon making any state change - either age or salary
 // all the components are being re-rendered (as evident from the logs)
 // so, we need to find a way that React re-renders only that component where the state change happens.
