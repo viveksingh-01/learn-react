424a1394cb4f34f1eaa307cd7f278f9b9fcf3e71
Vivek Singh <viveksingh9301@gmail.com>
2026-03-13 23:10:44 +0530

    Hooks: useRef - enhance Timer component to manage interval with useRef

--- src/Hooks/useRef/Timer.tsx
+++ src/Hooks/useRef/Timer.tsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Timer = () => {
   const [count, setCount] = useState(0);
+  const intervalRef = useRef<number | null>(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setCount((prevCount) => prevCount + 1);
     }, 1000);
-    return () => clearInterval(interval);
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
   }, []);
 
   return (
     <div>
       <h2>useRef Hook</h2>
       <p>{count}</p>
-      {/* <button onClick={() => clearInterval(interval)}>Stop</button> */}
+      <button
+        onClick={() => {
+          if (intervalRef.current) clearInterval(intervalRef.current);
+        }}
+      >
+        Stop
+      </button>
     </div>
   );
 };
@@ -28,3 +39,6 @@ export default Timer;
 // it will be declared over and over again with every re-render, and it's value will remain undefined.
 // So, we need a way to have access to the interval variable which persists through the re-renders.
 // The solution is useRef Hook!
+
+// useRef hook can not only be used to refer a DOM node, but also
+// it can be used to hold a reference to any mutable value, and make it persist through the re-renders.
